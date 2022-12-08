0d68879a852d1da56a962410061bb1aaca51a894
GILLIBON <95714441+GILLIBON@users.noreply.github.com>
2022-12-08 14:13:46 +0300

    Update Script4.py

--- Практическое занятие 3/Script4.py
+++ Практическое занятие 3/Script4.py
@@ -3,7 +3,7 @@ import cmath
 a = complex(input('Квадратное уравнение имеет следующий вид - ax^2+bx+c=0.\nВведите комплексное число a: '))
 b = complex(input('Введите комплексное число b: '))
 c = complex(input('Введите комплексное число c: '))
-d = b ** 2 - 4 * a * c
-x1 = (-b+cmath.sqrt(d)) / (2 * a)
-x2 = (-b-cmath.sqrt(d)) / (2 * a)
+d = b**2-4*a*c
+x1 = (-b+cmath.sqrt(d))/(2*a)
+x2 = (-b-cmath.sqrt(d))/(2*a)
 print('Число х1 =', x1,', число х2 =', x2)
