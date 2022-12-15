9d560c87e2c3305c83cb2f34600d491644549e6b
GILLIBON <95714441+GILLIBON@users.noreply.github.com>
2022-12-15 14:37:22 +0300

    Delete Практическое задание 5 directory

--- Практическое задание 5/Script1.py
+++
@@ -1,8 +0,0 @@
-def pwdcheck(pwd):
-    if len(pwd) >= 6 and any(sym.isdigit() for sym in pwd) and any(sym.isalpha() for sym in pwd) and "password" not in pwd.lower():
-        return True
-    else:
-        return False
-
-passw = str(input('Введите пароль: '))
-print(pwdcheck(passw))
--- Практическое задание 5/Script3.py
+++
@@ -1,9 +0,0 @@
-def fibon(n):
-    if n == 0 or n == 1:
-        fib = n
-    else:
-        fib = fibon(n - 1) + fibon(n - 2)
-    return(fib)
-
-print(fibon(3))
-
