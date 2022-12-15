ecf663fc2ef184577a70b0cdd5d714e4293343c8
GILLIBON <95714441+GILLIBON@users.noreply.github.com>
2022-12-15 14:37:01 +0300

    Add files via upload

---
+++ Практическое задание 5/Script1.py
@@ -0,0 +1,8 @@
+def pwdcheck(pwd):
+    if len(pwd) >= 6 and any(sym.isdigit() for sym in pwd) and any(sym.isalpha() for sym in pwd) and "password" not in pwd.lower():
+        return True
+    else:
+        return False
+
+passw = str(input('Введите пароль: '))
+print(pwdcheck(passw))
