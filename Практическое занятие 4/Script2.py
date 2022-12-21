20e35906fc64d40d247420d5946a99f676e6ee47
GILLIBON <95714441+GILLIBON@users.noreply.github.com>
2022-12-21 21:37:35 +0300

    Рефакторинг кода предыдущих дз с учётом PEP 8

--- Практическое занятие 4/Script2.py
+++ Практическое занятие 4/Script2.py
@@ -1,2 +1,6 @@
-dcolor = {'Red':(255,0,0), 'Orange':(255,140,0), 'Yellow':(255,255,0), 'Green':(0,255,0), 'Blue':(0,0,255), 'Violet':(255,0,255)}
-print ('RGB-код выбранного цвета:',dcolor[input('Выберите цвет из доступных\nRed, Orange, Yellow, Green, Blue, Violet: ')])
+dcolor = {'Red': (255, 0, 0), 'Orange': (255, 140, 0), 
+        'Yellow': (255, 255, 0), 'Green': (0, 255, 0), 
+        'Blue': (0, 0, 255), 'Violet': (255, 0, 255)
+        }
+
+print ('RGB-код выбранного цвета:', dcolor[input ('Выберите цвет из доступных\nRed, Orange, Yellow, Green, Blue, Violet: ')])
