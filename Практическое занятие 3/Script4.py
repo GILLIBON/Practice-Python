ed706e115a7c329c1adada0c71be6195083097a2
GILLIBON <95714441+GILLIBON@users.noreply.github.com>
2022-12-21 21:30:46 +0300

    Рефакторинг кода предыдущих дз с учётом PEP 8

--- Практическое занятие 3/Script4.py
+++ Практическое занятие 3/Script4.py
@@ -1,9 +1,12 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
 import cmath
+
 a = complex(input('Квадратное уравнение имеет следующий вид - ax^2+bx+c=0.\nВведите комплексное число a: '))
 b = complex(input('Введите комплексное число b: '))
 c = complex(input('Введите комплексное число c: '))
-d = b**2-4*a*c
-x1 = (-b+cmath.sqrt(d))/(2*a)
-x2 = (-b-cmath.sqrt(d))/(2*a)
+
+D = b**2 - 4*a*c
+
+x1 = (-b + cmath.sqrt(D)) / (2*a)
+x2 = (-b - cmath.sqrt(D)) / (2*a)
 print('Число х1 =', x1,', число х2 =', x2)
