17f7b4e2a4181f90b342a563e8af9f50d06ce7e4
GILLIBON <95714441+GILLIBON@users.noreply.github.com>
2022-12-21 20:59:33 +0300

    Рефакторинг кода предыдущих дз с учётом PEP 8

--- Практическое занятие 1/Упражнение 1.py
+++ Практическое занятие 1/Упражнение 1.py
@@ -2,10 +2,7 @@
 import math
 
 print("Теорема Пифагора - a²+b²=c²")
-print("Введите a:")
-a = float(input("a = "))
-print("Введите b:")
-b = float(input("b = "))
-
-c = math.sqrt(a*a+b*b)
+a = float( input("Введите a: ") )
+b = float( input("Введите b: ") )
+c = math.sqrt( a*a + b*b )
 print(f"C равно {c}")
