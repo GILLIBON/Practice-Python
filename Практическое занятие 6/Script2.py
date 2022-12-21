03b346871c7e3ed041f0379ad4a84f26e0d0e4f1
GILLIBON <95714441+GILLIBON@users.noreply.github.com>
2022-12-21 21:49:47 +0300

    Рефакторинг кода предыдущих дз с учётом PEP 8

--- Практическое занятие 6/Script2.py
+++ Практическое занятие 6/Script2.py
@@ -2,14 +2,14 @@ file = open('Input.txt', 'r')
 finput = file.read()
 
 c = int(finput[:finput.find(',')])
-finput = finput[finput.find(',')+1:]
+finput = finput[finput.find(',') + 1:]
 h = int(finput[:finput.find(',')])
 o = int(finput[:finput.find(','):-1])
 
 listout = []
-listout.append(c//2)
-listout.append((h-1)//5)
-listout.append(o//1)
+listout.append(c // 2)
+listout.append((h-1) // 5)
+listout.append(o // 1)
 
 out = str(min(listout))
 file = open('Output.txt', 'w')
