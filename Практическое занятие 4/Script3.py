abfa959d7995452d41ea6a2ad6a26c7c246cae3c
GILLIBON <95714441+GILLIBON@users.noreply.github.com>
2022-12-21 21:39:16 +0300

    Update Script3.py

--- Практическое занятие 4/Script3.py
+++ Практическое занятие 4/Script3.py
@@ -7,9 +7,9 @@ b = []
 for i in range(N):
     a.append(random.randint(1, 5))
     b.append(random.randint(1, 5))
-print ('Список 1:',a,'\nСписок 2:',b)
 
-print('Входят в оба списка:',list(set(a) & set(b)))
-print('Входят в первый, не входят во второй:',list(set(a) - set(b)))
-print('Входят в первый, не входят во второй:',list(set(b) - set(a)))
-print('Входят в первый или второй список, но не в оба:',list(set(a) ^ set(b)))
+print('Список 1:', a,'\nСписок 2:', b)
+print('Входят в оба списка:', list(set(a) & set(b)))
+print('Входят в первый, не входят во второй:', list(set(a) - set(b)))
+print('Входят в первый, не входят во второй:', list(set(b) - set(a)))
+print('Входят в первый или второй список, но не в оба:', list(set(a) ^ set(b)))
