65a36f254edd2e20539d3c1f99c365c91d8f3e66
GILLIBON <95714441+GILLIBON@users.noreply.github.com>
2022-12-21 21:35:06 +0300

    Рефакторинг кода предыдущих дз с учётом PEP 8

--- Практическое занятие 4/Script1.py
+++ Практическое занятие 4/Script1.py
@@ -1,16 +1,18 @@
 list1 = []
-listsize = int(input('Введите количество чисел в списке: '))
-print('Введите элементы списка:')
+listsize = int(input ('Введите количество чисел в списке: '))
+
+print('Введите элементы списка: ')
 
 for a in range(listsize):
-    list1.append(int(input()))
+    list1.append(int(input ()))
+
 print('Список, полученный при вводе: %s' %list1)
 
-for a in range(len(list1)):
+for a in range(len (list1)):
     a = 0
-    while a != len(list1)-1:
-        if list1[a] > list1[a+1]:
-            list1[a], list1[a+1] = list1[a+1], list1[a]
-        a = a+1
+    while a != len(list1) - 1:
+        if list1[a] > list1[a + 1]:
+            list1[a], list1[a + 1] = list1[a + 1], list1[a]
+        a += 1
 
 print ('Список после сортировки: %s' %list1)
