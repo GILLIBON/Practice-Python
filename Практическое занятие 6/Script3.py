cef7b9cd2be43534738b71826e1f3c9aa6f969ee
GILLIBON <95714441+GILLIBON@users.noreply.github.com>
2022-12-21 21:51:06 +0300

    Рефакторинг кода предыдущих дз с учётом PEP 8

--- Практическое занятие 6/Script3.py
+++ Практическое занятие 6/Script3.py
@@ -1,5 +1,4 @@
 msg = 'Привет, я обычное сообщение!'
-
 key  = 12341
 
 if key is not int:
@@ -10,13 +9,13 @@ if key > 999999:
     print('Ключ не может принимать более чем шестизначное значение. Установлено число 1.')
     key = 1
 
-def xor(msg,key):
+def xor(msg, key):
     xorstr = ''
     for i in msg:
         xorstr += chr(ord(i) ^ key)
     return xorstr    
  
-xorstr = xor(msg,key)
-print('Сообщение до шифрования: ',msg)
-print('Зашифрованное сообщение: ',xorstr)
-print('Расшифрованное сообщение: ',xor(xorstr,key))
+xorstr = xor(msg, key)
+print('Сообщение до шифрования: ', msg)
+print('Зашифрованное сообщение: ', xorstr)
+print('Расшифрованное сообщение: ', xor(xorstr,key))
